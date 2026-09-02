a7e8bfb149c0250ac0fc116f9e149bc0408d6655
Diana Cruz <dianacd1234567@gmail.com>
2026-09-02 08:13:41 -0600

    style: add decorative portfolio doodles

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,43 @@ import Education from './components/Education'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
+import raizDoodle from './assets/doodles/raiz.png'
+import narutoDoodle from './assets/doodles/naruto.png'
+import starDoodle from './assets/doodles/star.png'
+import strellaDoodle from './assets/doodles/strella.png'
+
 function App() {
   return (
     <>
+
+      <div className="doodle-layer" aria-hidden="true">
+        <img
+          src={raizDoodle}
+          alt=""
+          className="doodle doodle-raiz"
+          draggable="false"
+        />
+        <img
+          src={narutoDoodle}
+          alt=""
+          className="doodle doodle-naruto"
+          draggable="false"
+        />
+        <img
+          src={starDoodle}
+          alt=""
+          className="doodle doodle-star"
+          draggable="false"
+        />
+        <img
+          src={strellaDoodle}
+          alt=""
+          className="doodle doodle-strella"
+          draggable="false"
+        />
+      </div>
+
+
       <Navbar />
 
       <main>
